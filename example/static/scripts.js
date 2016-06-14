27be845188d7c5996cf60638591f7816ba907fe6
lcjdot <lwynn@searchdex.com>
2016-06-14 15:36:18 -0500

    Create scripts.js

---
+++ example/static/scripts.js
@@ -0,0 +1,44 @@
+// load images ajaxily and add to dom
+$.getJSON('images.json', function (images) {
+  var imageList = '';
+  $('#header').append(images.length + ' images')
+  $.each(images, function(i, src) {
+    imageList += '<li><img src="' + src + '" /></li>'
+  });
+  $('#image-list').append(imageList);
+});
+
+// load triggers ajaxily and check text for trigger words
+$.getJSON('triggers.json', function (triggers) {
+
+  var checktext = function (str) {
+    var arr = [], regex;
+    triggers.forEach( function(re) {
+      regex = (new RegExp(re));
+      if (str.match(regex) !== null) {
+        arr.push(str.match(regex)[0]);
+      }
+    });
+    return arr;
+  };
+
+  var dostuff = function (event) {
+    var $content = $('#regex-test-content');
+    var $result = $('#regex-test-result');
+
+    if (event.type === "change") {
+      var text = $content.html();
+      var results = checktext(text);
+      if (results.length > 0) {
+        $result.html('Triggers: <strong>' + results.join(', ') + '</strong>');
+      } else {
+        $result.html('No triggers found');
+      }
+
+    }
+
+  };
+
+  $('#regex-test').show().contentEditable().change(dostuff);
+
+});
